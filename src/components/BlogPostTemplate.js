6e93bcc99fd118c28c7dcd37c75c648ad144e2e1
Kevin Pennekamp <github@kevtiq.co>
2020-03-01 16:59:01 +0100

    more cleanup

--- src/components/BlogPostTemplate.js
+++ src/components/BlogPostTemplate.js
@@ -6,28 +6,26 @@ const BlogPostTemplate = ({ meta, children }) => {
   const twt = `${meta.title} by @kevtiq ${meta.url}`;
 
   return (
-    <main className="post">
-      <article>
-        <section className="post__header" role="contentinfo">
-          <h1>{meta.title}</h1>
-          <span className="post__meta">
-            <time dateTime={meta.date}>{meta.date}</time>
-            {` • ${formatReadingTime(meta.words)} • `}
-            <a href={`https://twitter.com/intent/tweet?text=${twt}`}>
-              share on twitter
-            </a>
-          </span>
-          {meta.tags && meta.tags.length > 0 && (
-            <TagList className="tags" tags={meta.tags} />
-          )}
-        </section>
+    <article className="post">
+      <header className="post__header" role="contentinfo">
+        <h1>{meta.title}</h1>
+        <span className="post__meta">
+          <time dateTime={meta.date}>{meta.date}</time>
+          {` • ${formatReadingTime(meta.words)} • `}
+          <a href={`https://twitter.com/intent/tweet?text=${twt}`}>
+            share on twitter
+          </a>
+        </span>
+        {meta.tags && meta.tags.length > 0 && (
+          <TagList className="tags" tags={meta.tags} />
+        )}
+      </header>
 
-        <section
-          className="post__body"
-          dangerouslySetInnerHTML={{ __html: children }}
-        />
-      </article>
-    </main>
+      <section
+        className="post__body"
+        dangerouslySetInnerHTML={{ __html: children }}
+      />
+    </article>
   );
 };
 

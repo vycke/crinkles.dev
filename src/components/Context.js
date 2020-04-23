dd7e701915e69620a154267aaa5364bbbd990add
Kevin Pennekamp <github@kevtiq.co>
2020-04-23 21:33:18 +0200

    context

--- src/components/Context.js
+++ src/components/Context.js
@@ -3,7 +3,11 @@ import React from 'react';
 export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  const [theme, setTheme] = React.useState(window.__theme || 'dark');
+  const [theme, setTheme] = React.useState('light');
+
+  React.useEffect(() => {
+    setTheme(window.__theme || 'light');
+  }, []);
 
   function updateTheme() {
     const newTheme = theme === 'dark' ? 'light' : 'dark';

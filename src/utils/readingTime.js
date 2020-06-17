10fcadbabfa6cd949a1b182a641a3c059bef1abf
Kevin Pennekamp <github@kevtiq.co>
2020-06-17 08:41:15 +0200

    details

--- src/utils/readingTime.js
+++ src/utils/readingTime.js
@@ -1,6 +1,3 @@
 export function formatReadingTime(words, wpm = 200) {
-  const min = Math.ceil(words / wpm);
-  let cups = Math.ceil(min / 3.5);
-  if (cups > 6) cups = 6;
-  return `${new Array(cups || 1).fill('☕️').join('')} ${min} min read`;
+  return `${Math.ceil(words / wpm)} min read`;
 }

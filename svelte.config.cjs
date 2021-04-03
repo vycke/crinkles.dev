b804ef60a2c64d09854efc434ff48f13c652e90b
Kevin Pennekamp <github@kevtiq.co>
2021-04-03 20:20:19 +0200

    changed build and deploy

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
+// const node = require('@sveltejs/adapter-node');
+const netlify = require('@sveltejs/adapter-netlify');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,8 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node(),
+		adapter: netlify(),
+		// adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',

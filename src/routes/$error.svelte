cc945467cfb7532b18612f7d83fbe619fb80680a
Kevin Pennekamp <github@crinkle.dev>
2021-06-19 20:02:07 +0200

    error page

--- src/routes/$error.svelte
+++ src/routes/$error.svelte
@@ -10,9 +10,13 @@
 	<title>🙈🙉🙊</title>
 </svelte:head>
 
-<Page class="place-center flow-y">
-	<Header />
-	<h1>{status} • {error.message}</h1>
+<Page class="text-center self-center">
+	{#if status && error}
+		<h1>{status} • {error?.message}</h1>
+	{/if}
+	{#if !(status && error)}
+		<h1>Something went wrong</h1>
+	{/if}
 	<h2>
 		<span role="img" aria-label="Sorry emoji" aria-labelledby=""> 🙈🙉🙊 </span>
 	</h2>

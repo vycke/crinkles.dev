593286b0f1151b44d226672f540eb42682668296
Kevin Pennekamp <github@crinkle.dev>
2022-02-03 15:15:07 +0100

    fix: for darkmode

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -8,19 +8,16 @@
 
 <style>
 	.card {
-    border-left: 5px solid var(--color-grey-4);
-    border-right: 5px solid var(--color-grey-4);
+    	border-left: 5px solid transparent;
+    	border-right: 5px solid transparent;
 		background-color: var(--color-grey-4);
 	}
-	.card:hover,
-	.card[data-type="dark"]:hover {
+	.card:hover {
 		border-left: 5px solid var(--color-primary);
 		border-right: 5px solid var(--color-primary);
 	}
 
 	.card[data-type="dark"] {
-    border-left: 5px solid var(--color-grey-5);
-    border-right: 5px solid var(--color-grey-5);
 		background-color: var(--color-grey-5);
 	}
 </style>

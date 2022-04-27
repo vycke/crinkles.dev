f95381521e0a3fee2a5bf18324bf045937300d17
Kevin Pennekamp <github@crinkle.dev>
2022-04-27 09:04:23 +0200

    feat: changed build

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		},
-		vite: {}
-	}
+		adapter: adapter()
+	},
+	vite: {}
 };
 
 export default config;

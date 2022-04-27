afb574b1b28c203c1db60e21338f8b581e15abf6
Kevin Pennekamp <github@crinkle.dev>
2022-04-27 09:08:24 +0200

    feat: changed build

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			default: true
+		}
 	},
 	vite: {}
 };

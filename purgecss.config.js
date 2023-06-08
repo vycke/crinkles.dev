e82f8fc38a56bc485be3261a356ff22ab6d32f3d
Kevin Pennekamp <github@crinkle.dev>
2023-06-08 09:36:54 +0200

    chore: package upgrades and cleanup

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-// purgecss.config.js
-
-module.exports = {
-  // Content files referencing CSS classes
-  content: ["./_site/**/*.html"],
-
-  // CSS files to be purged in-place
-  css: ["./_site/**/*.css"],
-};

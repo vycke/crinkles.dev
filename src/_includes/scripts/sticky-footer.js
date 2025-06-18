3aa74dda2d63c7ce55bc8c08251b0e4d0d192b61
Kevin Pennekamp <kevin@crinkles.dev>
2025-06-18 22:14:26 +0200

    fix: 🐛 color fixes

--- src/_includes/scripts/sticky-footer.js
+++ src/_includes/scripts/sticky-footer.js
@@ -1,5 +1,7 @@
-if (document && window) {
-  const footer = document.getElementById("site-footer");
-  if (document.body.offsetHeight - footer.offsetHeight > window.innerHeight)
-    footer.dataset.sticky = true;
-}
+(function () {
+  if (document && window) {
+    const footer = document.getElementById("site-footer");
+    if (document.body.offsetHeight - footer.offsetHeight > window.innerHeight)
+      footer.dataset.sticky = true;
+  }
+})();
